c28a2ae98de6e501b7ff901a439b082cb3c8fbbe
ICollado91 <collado.ip@gmail.com>
2016-10-22 01:49:42 -0400

    add next button functionality

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -7,6 +7,7 @@
         @type {Object}
         */
         var currentAlbum = Fixtures.getAlbum();
+        
         /**
         * @desc Buzz object audio file
         * @type {Object}
@@ -41,6 +42,17 @@
             currentBuzzObject.play();
             song.playing = true;
         };
+        
+        /**
+        * @function stopSong
+        * @desc Stop playing the current song
+        * @param {Object} song
+        */
+        var stopSong = function(song) {
+            currentBuzzObject.stop();
+            song.playing = null;
+        };
+        
         
         /**
         * @function getSongIndex
@@ -96,8 +108,25 @@
             currentSongIndex--;
             
             if (currentSongIndex < 0) {
-                currentBuzzObject.stop();
-                SongPlayer.currentSong.playing = null;
+                stopSong(SongPlayer.currentSong);
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
+        };
+        
+        /**
+        * @function next
+        * @desc Plays the next song
+        * @param {Object} song
+        */
+        SongPlayer.next = function () {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex++;
+            
+            if (currentSongIndex === currentAlbum.songs.length) {
+                stopSong(SongPlayer.currentSong);
             } else {
                 var song = currentAlbum.songs[currentSongIndex];
                 setSong(song);
